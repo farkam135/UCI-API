c15988f07fd26d520d5bb69d3c152643ac655e7b
Kamron Erani <kamronerani@gmail.com>
2017-11-04 12:17:20 -0700

    Ignore case for replacing &amp;

--- resources/ucisoc.js
+++ resources/ucisoc.js
@@ -332,7 +332,7 @@ function parseSOC(html) {
 
       currentCourse.dept = courseTitleArray[1].toUpperCase();
       currentCourse.num = courseTitleArray[2];
-      currentCourse.name = courseTitleArray[3].replace('&amp;','&');
+      currentCourse.name = courseTitleArray[3].replace(/&amp;/ig,'&');
     }
     //Course headers
     else if ($(tr).attr('bgcolor') === '#E7E7E7') {
