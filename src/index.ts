7097554f05bca303fc6fc9afefbf8ee6e59bb866
Kamron Erani <kamronerani@gmail.com>
2018-05-26 18:19:12 -0700

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,3 @@ export const UCI = {
   STUDENT: require("./resources/ucistudent"),
   PROFS: require("./resources/uciprofs")
 };
-
-//test.SOC.init()
-//.then(() => {
-//  return test.SOC.loadDept('COMPSCI');
-//})
-//.then(() => {
-//  console.log(JSON.stringify(test.SOC.getCourseDetails(['COMPSCI 122B'])));
-//})
-//UCI.STUDENT.login("kerani", "")
-//  .then((auth: string) => {
-//    return UCI.STUDENT.getCourses(auth);
-//  })
-//  .then((courses: string[]) => {
-//    console.log(JSON.stringify(courses));
-//  });
